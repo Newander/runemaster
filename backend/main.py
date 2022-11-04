ba9626669da342abcde741b9b5ef613eed9c0d16
Sergei Gavrilov <s.gavrilov@onesoil.ai>
2022-11-04 17:21:01 +0100

    Add graphs

--- backend/main.py
+++ backend/main.py
@@ -1,62 +1,81 @@
 from arango import ArangoClient
+from arango.graph import Graph
 
 
 class Task:
-    def __init__(self, name: str):
+    def __init__(self, pipeline_key: str, name: str):
+        self.pipeline_key = pipeline_key
         self.name = name
 
     def __rshift__(self, other):
         if not isinstance(other, Task):
             raise Exception('Unprocessable type!')
 
-        return TaskGraph(self) >> other
+        return TaskGraph(self.pipeline_key, init_task=self) >> other
 
 
 class TaskGraph:
-    def __init__(self, init_task: Task):
+    def __init__(self, pipeline_key: str, init_task: Task = None):
+        self.pipeline_key = pipeline_key
         if not isinstance(init_task, Task):
             raise Exception('Unprocessable type!')
 
-        self.task_ordered = [init_task]
-        self.steps = {0: [init_task]}
+        self.task_ordered: list[list[Task]] = []
+        self.steps = {0: []}
+
+        if init_task:
+            self.task_ordered.append([init_task])
+            self.steps[0].append(init_task)
 
     def __rshift__(self, other):
         if not isinstance(other, Task):
             raise Exception('Unprocessable type!')
 
-        self.task_ordered.append(other)
+        self.task_ordered.append([other])
         self.steps[max(self.steps) + 1] = [other]
+
+        return self
+
+    def upload(self, ar_graph: Graph):
+        task = ar_graph.vertex_collection("task")
+        edges = ar_graph.edge_collection("next")
+
+        for task_step in self.task_ordered:
+            task.insert()   # todo:
 
 
 class Pipeline:
     # todo: step 1:
-    def __init__(self, name: str):
+    def __init__(self, name: str, task_graph: TaskGraph = None):
         self.name = name
-        self.task_graphs = []
+        self.task_graph = task_graph or TaskGraph(pipeline_key=self.name)
 
-    def add(self, t: Task| TaskGraph):
+    def add(self, t: Task | TaskGraph):
         """ Добавляет одну таску или их цепочку / граф """
-        self.task_graphs.append(t)
+        self.task_graph >> t
 
     def dump(self):
         """ Записывает пайплайн в Арангу """
-        client = ArangoClient(hosts="http://localhost:8529")
-        db = client.db("test", username="root")
-        pipeline = db.create_collection("pipeline")
-        upd_pipe = pipeline.get({'name': self.name})
+        client = ArangoClient(hosts='http://localhost:8529')
+        db = client.db('test', username='root')
+        task_graph = db.graph("task_graph")
+        pipeline = db.collection('pipeline')
+        upd_pipe = pipeline.get({'_key': self.name})
 
-        result = upd_pipe.result()
-        pipeline.update()   # todo: stop here
-
+        self.task_graph.upload(task_graph)
+        if upd_pipe and upd_pipe.result():
+            pipeline.update({'_key': self.name, 'tasks': ...})
+        else:
+            pipeline.insert({'_key': self.name, 'tasks': ...})
 
 
 if __name__ == '__main__':
     # todo: Scenario - construct pipeline
-    download = Task('download')
-    sql_query = Task('sql_query')
-    upload = Task('upload')
-
     pipe = Pipeline('test_pipeline')
+    download = Task(pipe.name, 'download')
+    sql_query = Task(pipe.name, 'sql_query')
+    upload = Task(pipe.name, 'upload')
+
     pipe.add(download >> sql_query >> upload)
     pipe.dump()
 
--- deploy/ddl.py
+++ deploy/ddl.py
@@ -3,7 +3,19 @@ from arango import ArangoClient
 if __name__ == '__main__':
     client = ArangoClient(hosts="http://localhost:8529")
     sys_db = client.db("_system", username="root", password="passwd")
-    sys_db.create_database("test")
+
+    if not sys_db.has_database('test'):
+        sys_db.create_database("test")
 
     db = client.db("test", username="root")
-    db.create_collection("pipeline")
+
+    if not db.has_collection('pipeline'):
+        db.create_collection("pipeline")
+
+    task_graph = db.create_graph("task_graph")
+    task = task_graph.create_vertex_collection("task")
+    edges = task_graph.create_edge_definition(
+        edge_collection="next",
+        from_vertex_collections=["task"],
+        to_vertex_collections=["task"]
+    )
