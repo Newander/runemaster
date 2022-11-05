89f68050a12c6ba3133eaf0c3040c696085c8505
Sergei Gavrilov <s.gavrilov@onesoil.ai>
2022-11-05 15:37:06 +0100

    The example pipeline is ready

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
 from abc import ABC
 
 from arango import ArangoClient
+from arango.collection import VertexCollection
 from arango.graph import Graph
 
 
 class ArangoModuleMixin(ABC):
+
+    def __init__(self, *args, **kwargs):
+        self.record: dict | None = None  # Arango`s record dictionary
+
     @classmethod
     def from_arango(cls, ar_kwargs: dict):
         return cls(**{k: v for k, v in ar_kwargs.items() if k not in ('_id', '_key')})  # todo: check
@@ -13,13 +18,17 @@ class ArangoModuleMixin(ABC):
         """ Make a dictionary with all necessary for initialization arguments """
         raise NotImplementedError
 
-    def key(self):
+    def key(self) -> str:
         """ A special method returns [_key] attribute for arango record """
         raise NotImplementedError
+
+    def construct_record(self):
+        return {'_key': self.key(), **self.kwargs()}
 
 
 class Task(ArangoModuleMixin):
     def __init__(self, pipeline_key: str, name: str):
+        super().__init__()
         self.pipeline_key = pipeline_key
         self.name = name
 
@@ -34,6 +43,10 @@ class Task(ArangoModuleMixin):
 
     def key(self):
         return f'{self.pipeline_key}_{self.name}'
+
+    def insert(self, ar_task: VertexCollection) -> dict:
+        self.record = ar_task.insert({'_key': self.key(), **self.kwargs()})
+        return self.record
 
 
 class TaskGraph:
@@ -50,7 +63,7 @@ class TaskGraph:
             self.steps[0].append(init_task)
 
     def __rshift__(self, other):
-        if isinstance(other, (Task)):
+        if isinstance(other, Task):
             self.task_ordered.append([other])
             self.steps[max(self.steps) + 1] = [other]
             return self
@@ -73,8 +86,8 @@ class TaskGraph:
         for task_step in self.task_ordered:
             now_task_ar = []
             for task_instance in task_step:
-                new_task = task.insert({'_key': task_instance.key(), **task_instance.kwargs()})
-                now_task_ar.append(new_task)
+                ar_task = task_instance.insert(task)
+                now_task_ar.append(ar_task)
 
             for prev_ar_task in prev_tasks_ar:
                 for now_ar_task in now_task_ar:
@@ -83,11 +96,19 @@ class TaskGraph:
             prev_tasks_ar = now_task_ar
 
 
-class Pipeline:
-    # todo: step 1:
+class Pipeline(ArangoModuleMixin):
     def __init__(self, name: str, task_graph: TaskGraph = None):
+        super().__init__()
         self.name = name
         self.task_graph = task_graph or TaskGraph(pipeline_key=self.name)
+
+    # todo: step 1:
+    def kwargs(self):
+        return {'name': self.name,
+                'tasks': [[task.record for task in task_group] for task_group in self.task_graph.task_ordered]}
+
+    def key(self):
+        return f'{self.name}'
 
     def add(self, t: Task | TaskGraph):
         """ Добавляет одну таску или их цепочку / граф """
@@ -101,11 +122,11 @@ class Pipeline:
         pipeline = db.collection('pipeline')
         upd_pipe = pipeline.get({'_key': self.name})
 
-        self.task_graph.upload(task_graph)
+        self.task_graph.upload(task_graph)  # todo: need to insert into self.kwargs in some way..?
         if upd_pipe and upd_pipe.result():
-            pipeline.update({'_key': self.name, 'tasks': ...})
+            pipeline.update(self.construct_record())
         else:
-            pipeline.insert({'_key': self.name, 'tasks': ...})
+            pipeline.insert(self.construct_record())
 
 
 if __name__ == '__main__':
